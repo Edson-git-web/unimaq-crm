85d72528d5572ce2493ed902a06690d9a42304e7
Edson-git-web <edsonmanuelmartinezcarrizales@gmail.com>
2026-06-18 17:04:52 -0500

    fix: excluir password de log de auditoria (H-005)

--- app/Services/AuditoriaService.php
+++ app/Services/AuditoriaService.php
@@ -9,6 +9,13 @@ class AuditoriaService
 {
     public static function registrar($accion, $tabla_afectada = null, $registro_id = null, $datos_antes = null, $datos_despues = null)
     {
+        if (is_array($datos_antes) && array_key_exists('password', $datos_antes)) {
+            unset($datos_antes['password']);
+        }
+        if (is_array($datos_despues) && array_key_exists('password', $datos_despues)) {
+            unset($datos_despues['password']);
+        }
+
         Auditoria::create([
             'id_usuario' => Auth::id(),
             'accion' => $accion,
